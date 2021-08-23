d298194521efd0410e1021d27b1308c47090d38d
Erik Jensen <ejensen@netradius.com>
2021-08-23 10:16:13 -0600

    Fixing helper script

--- helper.sh
+++ helper.sh
@@ -44,9 +44,9 @@ function aws_authentication() {
   if ! command -v aws &> /dev/null; then echo "aws command missing"; exit 1; fi
   if ! command -v jq &> /dev/null; then echo "jq command missing"; exit 1; fi
   if [[ -n "${AWS_OIDC_ROLE_ARN+x}" ]]; then
-    _oidc_auth
+    _oidc_authentication
   else
-    _default_auth
+    _default_authentication
   fi
   _aws_assume_role
 }
