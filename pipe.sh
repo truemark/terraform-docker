5a604e170fcd9ea0f8740351b098aa088764377b
Erik Jensen <ejensen@netradius.com>
2021-08-23 12:05:52 -0600

    Adding some debug

--- pipe.sh
+++ pipe.sh
@@ -8,6 +8,9 @@ git_crypt_unlock
 
 AWS_ACCOUNT_ID="$(aws_current_account_id)"
 export AWS_ACCOUNT_ID
+if [[ "${DEBUG+X}" == "true" ]]; then
+  echo "Current AWS Account is \"${AWS_ACCOUNT_ID}\""
+fi
 
 COMMAND=${COMMAND:?'COMMAND variable is required'}
 for CMD in "${!COMMAND@}"; do
