7d15b0ef200707fab93e7bc012c1d58a850a7876
Erik Jensen <365211+erikrj@users.noreply.github.com>
2023-05-31 11:21:34 -0600

    Fixing trailing spaces

--- version.sh
+++ version.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 export TERRAFORM_VERSION=$(curl -sSLf https://checkpoint-api.hashicorp.com/v1/check/terraform | jq -r .current_version)
-export TERRAFORM_MINOR_VERSION=$(echo "${TERRAFORM_VERSION}" | cut -d '.' -f 1,2 )
+export TERRAFORM_MINOR_VERSION=$(echo "${TERRAFORM_VERSION}" | cut -d '.' -f 1,2)
